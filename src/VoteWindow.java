083f375c5f362dc241c43563f0d39b8dd0d8aba4
a-fitzg <xanderfitzg2000@gmail.com>
2020-09-01 00:15:45 +1000

    Further improvements to vote window UI, with event handler stubs for onClick event

--- src/VoteWindow.java
+++ src/VoteWindow.java
@@ -1,13 +1,13 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -29,7 +29,9 @@ public class VoteWindow extends Application {
             new DummyCandidate(2, "Anthony Albanese", "Australian Labor Party"),
             new DummyCandidate(3, "Adam Bandt", "Australian Greens"),
             new DummyCandidate(4, "Pauline Hanson", "Pauline Hanson's One Nation"),
-            new DummyCandidate(5, "Robbie Katter", "Katter's Australian Party")
+            new DummyCandidate(5, "Robbie Katter", "Katter's Australian Party"),
+            new DummyCandidate(6, "Jacqui Lambie", "Jacqui Lambie Network"),
+            new DummyCandidate(7, "Robert Brown", "Shooters, Fishers and Farmers Party")
     );
 
     @Override
@@ -37,7 +39,7 @@ public class VoteWindow extends Application {
 
         Scene scene = new Scene(new Group());
         stage.setTitle("Place Vote");
-        stage.setWidth(620);
+        stage.setWidth(700);
         stage.setHeight(600);
 
         scene.getStylesheets().add("styles/styles.css");
@@ -52,37 +54,76 @@ public class VoteWindow extends Application {
 
         GridPane votePane = new GridPane();
         votePane.setPrefWidth(stage.getWidth());
-        votePane.setHgap(10);
-        votePane.setVgap(10);
+        votePane.setHgap(5);
+        votePane.setVgap(5);
+        votePane.setPadding(new Insets(0, 5, 0, 5));
 
         // Each "DummyCandidate" object is assigned a TextArea, which can be changed when
         // user changes their vote
-        Map<DummyCandidate, TextArea> preferenceBoxMap = new HashMap<>();
+        Map<DummyCandidate, Label> preferenceBoxMap = new HashMap<>();
+
+        // Each "DummyCandidate" object is also assigned a box that, when clicked, will register
+        // a vote for that candidate
+        Map<DummyCandidate, HBox> voteCardMap = new HashMap<>();
 
         // Iterates through all the candidates and displays them on the screen
         // Do not use a for-each loop here, we need a numeric index
         for (int i = 0; i < candidateList.size(); i++) {
-            TextArea preferenceBox = new TextArea();
-            preferenceBox.setText("1");
-            preferenceBox.setPrefSize(20, 20);
-            preferenceBox.getStyleClass().add("input-vote");
+            Label preferenceLabel = new Label();
+            //preferenceLabel.setText("1");
+            preferenceLabel.getStyleClass().add("preference-label");
+            preferenceLabel.setPrefSize(50, 50);
 
             // Here, the DummyCandidate is assigned a TextArea
-            preferenceBoxMap.put(candidateList.get(i), preferenceBox);
+            // (the box with the preference number inside)
+            preferenceBoxMap.put(candidateList.get(i), preferenceLabel);
 
             Text candidateName = new Text(candidateList.get(i).getCandidate());
             Text candidateParty = new Text(candidateList.get(i).getParty());
+
             VBox candidateVbox = new VBox();
             candidateVbox.getChildren().addAll(candidateName, candidateParty);
             candidateVbox.getStyleClass().add("vote-candidate-display");
+            candidateVbox.setPadding(new Insets(0, 10, 0, 10));
+
             HBox voteCard = new HBox();
             voteCard.setPrefWidth(stage.getWidth());
             voteCard.getStyleClass().add("vote-card");
-            voteCard.getChildren().addAll(preferenceBox, candidateVbox);
+            voteCard.getChildren().addAll(preferenceLabel, candidateVbox);
+
+            // Shadow to make the cards look a bit more pretty and professional
+            DropShadow cardShadow = new DropShadow();
+            cardShadow.setRadius(2.0);
+            cardShadow.setOffsetX(1.0);
+            cardShadow.setOffsetY(1.0);
+            cardShadow.setColor(Color.color(0.5, 0.5, 0.5));
+            voteCard.setEffect(cardShadow);
+
+            // We also assign each candidate a vote "card" (just an HBox)
+            voteCardMap.put(candidateList.get(i), voteCard);
 
             // This is why we need the numeric index, every other candidate is put onto a new line
             votePane.add(voteCard, i % 2, i / 2);
+
+            // Event handler for when the card is clicked, and the user votes/unvotes for this candidate
+            voteCard.setOnMouseClicked(mouseEvent -> {
+                // TODO Implement click vote functionality, and reset preference field
+
+                // You can reset this card's preference number with the following:
+                // preferenceLabel.setText(NEW_NUMBER_HERE);
+
+                // You can also use candidateList.get(i) to get this candidate, but pls
+                // replace the "DummyCandidate" class with the actual proper candidate class
+
+                // If you want to change the preference number outside of this function, then
+                // use the preferenceBoxMap to access the Label associated with a candidate,
+                // then use .setText(NEW_NUMBER_HERE)
+            });
+
         }
+
+        // Spacer between vote options and buttons
+        Region spacer = new Region();
 
         // Button pane
         Button clearButton = new Button("Clear all");
@@ -92,12 +133,9 @@ public class VoteWindow extends Application {
         confirmButton.getStyleClass().add("confirm-button");
 
         HBox buttonRow = new HBox(clearButton, confirmButton);
-
         buttonRow.setPrefWidth(200);
-
-        buttonRow.setSpacing(10);
-        //buttonRow.setMargin(clearButton, new Insets(10, 10, 10, 10));
-        //buttonRow.setMargin(confirmButton, new Insets(10, 10, 10, 10));
+        buttonRow.setSpacing(5);
+        buttonRow.setPadding(new Insets(0, 5, 0, 5));
 
         clearButton.setPrefWidth((stage.getWidth() - 20) / 2);
         confirmButton.setPrefWidth((stage.getWidth() - 20) / 2);
@@ -107,7 +145,8 @@ public class VoteWindow extends Application {
 
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
-        vbox.getChildren().addAll(titleBox, votePane, buttonRow);
+        VBox.setVgrow(spacer, Priority.ALWAYS);
+        vbox.getChildren().addAll(titleBox, votePane, spacer, buttonRow);
 
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
 
