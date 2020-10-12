df319591a5b6381d061920f436f7ee4149678c1a
samtudor0112 <samueltudor@hotmail.com>
2020-10-13 09:05:02 +1000

    added a config class, WIP

---
+++ src/evm/Config.java
@@ -0,0 +1,12 @@
+package evm;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A class that stores the data read in the config files
+ */
+public class Config {
+    List<Ballot> ballots;
+    Map<String, Integer> extraData;
+}
